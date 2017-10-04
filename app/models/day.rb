4b5cde27961adcc3fbef92d310922822cfa6cccb
Zoë Sparks <zoe@milky.flowers>
2017-10-04 05:15:20 -0400

    Set up associations for Day and TaskGroup

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,6 @@
 class Day < ApplicationRecord
+  has_many :task_groups
+
   def pretty_calendar
     calendar.strftime "%A, %B %-d, %Y"
   end
--- app/models/task_group.rb
+++ app/models/task_group.rb
@@ -1,3 +1,5 @@
 class TaskGroup < ApplicationRecord
   belongs_to :day
+  has_many   :tasks
+  has_one    :scratch
 end
